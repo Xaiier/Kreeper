0fa151d65c830c2d974d7302fe1a6b3dfd0bbebe
Xaiier <warrenkcasey@gmail.com>
2014-09-02 19:23:08 -0400

    setup commit

---
+++ Kreeper/Kreeper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+using KSP.IO;
+
+namespace Kreeper
+{
+    [KSPAddon(KSPAddon.Startup.Instantly, true)]
+    public class Kreeper : MonoBehaviour
+    {
+    }
+}
